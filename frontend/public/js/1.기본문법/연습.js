a649215db0b8a110f4609ffba6a1ae0f41d22366
백한결 <gksruf1002@naver.com>
2023-01-10 01:56:24 +0900

    1/9일 연습

---
+++ frontend/public/js/1.기본문법/연습.js
@@ -0,0 +1,128 @@
+
+/*console.log('js시작합니다')
+console.log('Hello world')
+console.log("hello world")
+console.log(200)
+console.log('200')
+console.log( true )
+
+console.log( '//\n||' )
+console.log( '//\t||' )
+
+
+console.log('안녕하세요\\js처음입니다')
+console.log(' 안녕하세요\'js처음입니다\'')
+
+console.log('안녕하세\\"js처음입니다\"//')
+
+alert('hello world')
+alert("hello world")
+alert('200+200')
+alert(true)
+*/
+
+
+/*
+let value1=10
+console.log('변수값:'+value1)
+
+value1=20
+console.log('변수값 변경후:'+value1)
+
+console.log('변수값5더하기:'+value1+5)
+
+console.log('변수값5더하기:'+(value1+5))
+
+/*
+let value2=30
+let result=value1+value2
+console.log('변수들간의 연산:'+result)
+
+
+const data1=10
+console.log('상수값:'+data1)
+
+const data2=20
+console.log('상수값2:'+data2)
+const result2=data1+data2
+console.log('상수들간의 연산:'+result2)
+ */
+
+/*console.log(10+3)
+console.log('더하기:'+10+3)
+console.log('더하기:'+(10+3))
+console.log('빼기:'+(10-3))
+console.log('곱하기:'+(3*10))
+console.log('나누기:'+(10/3))
+console.log('나머지:'+(10%3))
+
+console.log('초과:'+(10>3))
+console.log(10<3)
+console.log(10>=3)
+console.log(10<=3)
+console.log(10==3)
+console.log(10!=3)
+
+console.log(10>3&&10>5)
+console.log(10>3||3>5)
+console.log(!(10>3))
+*/
+
+/*let 국어=Number(prompt('국어값'))
+let 수학=Number(prompt('수학값'))
+console.log('총점:'+(국어+수학))
+console.log('평균:'+(국어+수학)/2)
+
+*/
+/*
+let 반지름=Number(prompt('반지름'))
+console.log((반지름*반지름*3.14))
+*/
+
+/*let 실수1=Number(prompt('실수1'))
+let 실수2=Number(prompt('실수2'))
+
+console.log('몇%:'+(실수1/실수2)*100+'%')*/
+
+/*let 정수1=(prompt('정수1'))
+console.log(정수1%2==1)
+*/
+
+/*
+let 정수2=Number(prompt('정수2'))
+console.log(정수2%7==0)*/
+
+/*let 정수3=Number(prompt('정수3'))
+console.log(정수3%2==1 && 정수3%7==0)*/
+
+/*
+let 아이디=(prompt('아이디'))
+let 비밀번호=(prompt('비밀번호'))
+console.log((아이디=='admin') && (비밀번호=='1234'))*/
+
+
+/*let 아이디 =(prompt ('아이디'))
+let 비밀번호=(prompt('비밀번호'))
+
+console.log((아이디=='admin')&&(비밀번호=='1234'))
+
+*/
+
+/*let result1 = confirm('실행하시겠습니까?')
+console.log( '결과1 : ' + result1 )
+
+
+let result2 = prompt('데이터 입력')
+console.log( '결과2 : ' + result2 )
+
+*/
+
+let value4 = Number( prompt('정수4') )
+let value5 = Number( prompt('정수5') )
+let value6 = Number( prompt('정수6') )
+let max = value4
+max = max < value5 ? value5 : max
+max = max < value6 ? value6 : max
+console.log( ' 가장 큰 수는 : ' + max )
+
+
