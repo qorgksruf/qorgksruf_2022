9f1c8dce7af2e32d20fbec5f805974821db3fc1c
백한결 <gksruf1002@naver.com>
2023-01-15 21:58:41 +0900

    1/15과제 최종수정

--- frontend/public/js/0.과제/과제4/과제4.js
+++ frontend/public/js/0.과제/과제4/과제4.js
@@ -10,50 +10,56 @@ function rental(){
 	
 	console.log('rental함수 실행;')
 
-		let sno = document.querySelector('.sno').value;
-		
-			let snoValue = sno.value;
-				console.log('5번체크 : '+  snoValue ) 
+	let sno = document.querySelector('.sno').value;	// 책태그 정보
+	
+	console.log("sno의 태그값 ::: ");
+	console.log(sno);
 					
-					let sIndex =  도서목록.indexOf( snoValue )
-						console.log('6번체크 : ' + sIndex )
-						
-							if(sIndex >= 0){
-								document.querySelector('.resultbox').innerHTML="대여가능합니다"
-								alert('대여되었습니다')
-							 }	
-						 	else if( sIndex == -1){
-								document.querySelector('.resultbox').innerHTML="대여불가합니다" 
-								alert('대여불가합니다')		
-							}
-							else{document.querySelector('.resultBox').innerHTML = "없는 책 입니다" }
-								alert('없는 책입니다')
+	let sIndex =  도서목록.indexOf( sno );
+	console.log('6번체크 : ' + sIndex )
+	
+	if (sIndex >= 0) {
+		console.log("해당 책이 존재합니다.")
+		if(대여목록.indexOf(sIndex) >= 0) {
+			alert("해당 책은 대여 중입니다.");
+		} else {
+			대여목록.push(sIndex);
+			alert("해당 책을 대여 완료하였습니다.");
+		}
+	} else {
+		alert("해당 책이 존재하지 않습니다.")
+	}
+	
+
 }
 
 
 
 function derental(){
 	
-	console.log('derental함수 실행;')
+	console.log('derental함수 실행');
 
-		let sno = document.querySelector('.sno').value;
+	let sno = document.querySelector('.sno');
 		
-			let snoValue = sno.value;
-				console.log('5번체크 : '+  snoValue ) 
+	let snoValue = sno.value;
+	console.log('5번체크 : '+  snoValue );
 					
-					let sIndex =  도서목록.indexOf( snoValue )
-						console.log('6번체크 : ' + sIndex )
-						
-							if(sIndex >= 0){
-								document.querySelector('.resultbox').innerHTML="대여가능합니다"
-								alert('대여되었습니다')
-							 }	
-						 	else if( sIndex == -1){
-								document.querySelector('.resultbox').innerHTML="대여불가합니다" 
-								alert('대여불가합니다')		
-							}
-							else{document.querySelector('.resultBox').innerHTML = "없는 책 입니다" }
-								alert('없는 책입니다')
+	let sIndex =  도서목록.indexOf( snoValue );
+	console.log('6번체크 : ' + sIndex );
+	
+	if (sIndex >= 0) {
+		console.log("해당 책이 존재합니다.")
+		if(대여목록.indexOf(sIndex) >= 0) {
+			대여목록.splice(대여목록.indexOf(sIndex), 1);
+			alert("해당 책을 반납 완료하였습니다.");
+		} else {
+			alert("해당 책은 현재 대여가능한 책으로 반납이 불가합니다.");
+		}
+	} else {
+		alert("해당 책이 존재하지 않습니다.")
+	}
+	
+	
 }
 
-
+		/* 대여상태부분 바뀌는거 못했습니다 splice이용해서 했던것같은데....*/
