e4112d03cab44526de384c8dd07c97e7e69fc884
504 <504@DESKTOP-4QHEEJ3>
2023-02-16 17:20:24 +0900

    ㅇㅇ

---
+++ backend/src/미니팀프로젝트/Model/회의실Model.java
@@ -0,0 +1,12 @@
+package 미니팀프로젝트.Model;
+
+public class 회의실Model {
+	   String name;
+	   String content;
+	   @Override
+	   public String toString() {
+	      return "작성자 이름 : " + name + "\t"+ " 내용 : " + content;
+	   }
+	   
+	   
+	}
